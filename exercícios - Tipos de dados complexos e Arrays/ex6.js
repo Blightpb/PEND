a3a3483dd6ab00eba4c16a4edd6b6bfe8b33210c
Rafaela Silva da Paz <159180376+Blightpb@users.noreply.github.com>
2025-03-11 15:55:40 -0300

    Add files via upload

---
+++ exercícios - Tipos de dados complexos e Arrays/ex6.js
@@ -0,0 +1,5 @@
+let filmes = ["Matrix", "Inception", "Avatar"];
+
+console.log(filmes); //exibe os filmes
+filmes.shift();// remove o primeiro filme
+console.log(filmes);//exibe sem o primeiro filme 

b4aed81e8a7a97c0a7dd6d6ea6ca7d97a5190045
Rafaela Silva da Paz <159180376+Blightpb@users.noreply.github.com>
2025-03-11 15:43:55 -0300

    Add files via upload

---
+++ exercícios - Tipos de dados complexos e Arrays/ex3.js
@@ -0,0 +1,5 @@
+let cores = ["vermelho", "azul"]
+
+console.log(cores); //exibe as cores declaradas 
+cores.push("verde"); // adiciona a cor verde
+console.log(cores); //exibe como fica depois de adicionar o verde

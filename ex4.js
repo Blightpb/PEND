5d8f5cda10ce807f4245b13806685212f43f4969
Rafaela Silva da Paz <159180376+Blightpb@users.noreply.github.com>
2025-03-11 15:46:23 -0300

    Add files via upload

---
+++ ex4.js
@@ -0,0 +1,5 @@
+let animais = ["cachorro", "gato"]
+
+console.log(animais); //exibe os animais declarados 
+animais.unshift("papagaio"); // adiciona o papagaio em primeiro lugar
+console.log(animais); //exibe como fica depois de adicionar o papagaio

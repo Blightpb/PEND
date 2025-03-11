3b4b2e4f8a80216e2e01c6da7712c3140672fe28
Rafaela Silva da Paz <159180376+Blightpb@users.noreply.github.com>
2025-03-11 15:46:56 -0300

    Delete ex4.js

--- ex4.js
+++
@@ -1,5 +0,0 @@
-let animais = ["cachorro", "gato"]
-
-console.log(animais); //exibe os animais declarados 
-animais.unshift("papagaio"); // adiciona o papagaio em primeiro lugar
-console.log(animais); //exibe como fica depois de adicionar o papagaio
